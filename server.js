a5b198579c53b030441d5fce8e90c74da901a22d
jithinjof-hub <jithinjof@gmail.com>
2026-07-18 16:20:24 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'dist'))); // അല്ലെങ്കിൽ നിങ്ങളുടെ frontend ബിൽഡ് ഫോൾഡർ
+app.use(express.static(path.join(__dirname, 'dist'))); 
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
